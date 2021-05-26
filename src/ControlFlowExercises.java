cf3a7d5680ade3f254821adeee9fbdab9d12cc7d
Juan Garcia <garcia.d.juan14@gmail.com>
2021-05-25 21:13:37 -0500

    finished 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -40,20 +40,51 @@ public class ControlFlowExercises {
 
 // question 2
         Scanner scanner = new Scanner(System.in);
-        final int PIN = 12345;
-        for (int i = 1; i <= 3; i++) {
-            System.out.println("Please enter pin:");
-            int userInputPin = scanner.nextInt();
-            if (userInputPin != PIN) {
-                System.out.println("Incorrect, try again.");
-            } else if (userInputPin == PIN) {
-                System.out.println("Correct, welcome back.");
-                break;
-            } else if (i == 3 && userInputPin != PIN) {
-                System.out.println("Sorry but you have been locked out");
+//        final int PIN = 12345;
+//        for (int i = 1; i <= 3; i++) {
+//            System.out.println("Please enter pin:");
+//            int userInputPin = scanner.nextInt();
+//            if (userInputPin != PIN) {
+//                System.out.println("Incorrect, try again.");
+//            } else if (userInputPin == PIN) {
+//                System.out.println("Correct, welcome back.");
+//                break;
+//            } else if (i == 3 && userInputPin != PIN) {
+//                System.out.println("Sorry but you have been locked out");
+//            }
+//        }
+// question 3
+
+        int userInt = 0;
+        do {
+
+
+            System.out.print("What number would you like see on the table? (-1 cancels ends the loop)");
+            userInt = scanner.nextInt();
+            System.out.println("");
+
+            if(userInt == -1) {
+                System.out.println("thank you come again!");
+            } else {
+                System.out.println("Here is your table!");
+                System.out.println("");
+                System.out.println("number | squared | cubed");
+                System.out.println("------ | ------- | -----");
+                for (int i = 1; i <= userInt; i++) {
+                    System.out.println(i + "\t   | " + (i * i) + "\t     |" + "  " + (i * i * i));
+                }
             }
         }
-// question 3
+        while (userInt != -1);
+
+
+// question 4
+
+
+//        for (int i = 0; 1 <= 8; i++) {
+//            System.out.println("Enter your grades (0 - 100) for this period.");
+//            int gradeInput = scanner.nextInt();
+//        }
 
 
     }
