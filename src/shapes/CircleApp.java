47c0ab604f41e96703834dcc2306d3d05740fa83
Juan Garcia <garcia.d.juan14@gmail.com>
2021-06-02 10:47:19 -0500

    minor changes

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -8,7 +8,7 @@ public class CircleApp {
     public static void main(String[] args) {
         Input test = new Input();
         Circle circle = new Circle(test.getDouble());
-        DecimalFormat f = new DecimalFormat("#.##");
+        DecimalFormat f = new DecimalFormat("0.00");
         System.out.println("The area of the circle is: " + f.format(circle.getArea()));
         System.out.println("The circumference of the circle is: " + f.format(circle.getCircumference()));
     }
